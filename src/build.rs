0298cc58cba340a22943e6be3f3d8d83f455d866
Stephan Hügel <urschrei@gmail.com>
2016-02-06 12:28:33 +0000

    Beautify

--- src/build.rs
+++ src/build.rs
@@ -6,33 +6,24 @@ use std::io::BufWriter;
 
 const GENERATED_FILE: &'static str = "src/ostn02.rs";
 
-
 fn main() {
     let keys = vec!["13928b", "13928c", "13a28b", "13a28c"];
     let values: Vec<(_, _, _)> = vec![(16500, 3359, 270),
-                                          (16538, 3357, 254),
-                                          (16508, 3387, 258),
-                                          (16547, 3376, 242)];
-    // let ostn02 = keys.drain(..).zip(values.drain(..)).collect::<HashMap<_, (_, _, _)>>();
+                                      (16538, 3357, 254),
+                                      (16508, 3387, 258),
+                                      (16547, 3376, 242)];
 
-
-
-
-    let ostn02: Vec<_> = keys.iter().zip(values.iter())
-        // .map(|&(k, v)| (k, v))
-        .collect();
-
+    let ostn02: Vec<_> = keys.iter()
+                             .zip(values.iter())
+                             .collect();
     let mut outfile = BufWriter::new(File::create(GENERATED_FILE).unwrap());
-
-    write!(outfile, "static OSTN02: phf::Map<&'static str, (i32, i32, i32)> = ").unwrap();    
-    
+    write!(outfile,
+           "static OSTN02: phf::Map<&'static str, (i32, i32, i32)> = ")
+        .unwrap();
     let mut map = phf_codegen::Map::<&str>::new();
-
     for &(ref key, val) in &ostn02 {
         map.entry(key, &format!("{:?}", val));
     }
-
     map.build(&mut outfile).unwrap();
-
     writeln!(outfile, ";").unwrap();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,9 @@
 extern crate phf;
 include!("ostn02.rs");
 
-#[test]
-fn it_works() {
-}
-pub fn ostn02_lookup(key: &str) -> Option<(i32, i32, i32)>  {
-    if key.is_empty() { return None; }
-    OSTN02.get(&*key).cloned()  
+pub fn ostn02_lookup(key: &str) -> Option<(i32, i32, i32)> {
+    if key.is_empty() {
+        return None;
+    }
+    OSTN02.get(&*key).cloned()
 }
