ea248539f77152974428b8147fc0464d4b459251
Stephan Hügel <stephan.hugel.12@ucl.ac.uk>
2016-02-08 13:06:38 +0000

    Typo fix and keyword addition

--- src/lib.rs
+++ src/lib.rs
@@ -5,14 +5,14 @@ extern crate phf;
 include!("ostn02.rs");
 
 /// Return a 3-tuple of Easting, Northing, and height adjustments, for use in transforming
-/// ETRS89 Eastings and Northings to OSGB36 Eastings, Northings
+/// ETRS89 Eastings and Northings to OSGB36 Eastings, Northings.  
 /// The key is the combined hex-transformed (03x) kilometer-grid reference of the Northings and Eastings:
 /// 
 /// # Examples
 /// 
 /// ```
 /// use ostn02_phf::ostn02_lookup;
-///
+/// 
 /// // Caister Tower Eastings and Northings: 651307.003, 313255.686
 /// let e_grid = (651307.003 / 1000.) as i32;
 /// let n_grid = (313255.686 / 1000.) as i32;
